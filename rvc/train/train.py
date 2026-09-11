7f5d80608e6d7ef1bd9170c69058b56347df7a57
redpanda343 <aari200324@gmail.com>
2026-09-11 13:00:41 -0300

    Remove slop

--- rvc/train/train.py
+++ rvc/train/train.py
@@ -1109,9 +1109,7 @@ def train_and_evaluate(
                 with torch.amp.autocast(
                     device_type="cuda", enabled=use_amp, dtype=train_dtype
                 ):
-                    y_d_hat_r, y_d_hat_g, _, _ = net_d(
-                        wave, y_hat.detach(), combine_inputs=True
-                    )
+                    y_d_hat_r, y_d_hat_g, _, _ = net_d(wave, y_hat.detach())
                 loss_disc, _, _ = discriminator_loss(y_d_hat_r, y_d_hat_g)
                 # Discriminator backward and update
                 optim_d.zero_grad()
